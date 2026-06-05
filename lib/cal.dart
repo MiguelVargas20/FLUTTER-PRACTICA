4de8417f5815878cebc3558b06072d87adafa8ff
MiguelVargas20 <andresvarg150@gmail.com>
2026-06-04 21:11:22 -0500

    feaure: Desarrollo calculadora con operadores funcionales pantalla

--- lib/cal.dart
+++ lib/cal.dart
@@ -9,6 +9,10 @@ State<Calculadora> createState() => _CalculadoraState();
 
 class _CalculadoraState extends State<Calculadora>{
   String operacion ="";
+  String resultado ="0";
+  String numeroActual = "";
+  List<String> elementos = [];
+
 
 
 //
@@ -25,21 +29,21 @@ class _CalculadoraState extends State<Calculadora>{
               width: double.infinity,
 
               //20 se refiere a puntos no a pixeles (medida usada en desarrollo móvil)
-              padding: const EdgeInsets.all(20),
+              padding: const EdgeInsets.all(40),
               child: Column(
                 //Alinea al ancho
                 mainAxisAlignment: MainAxisAlignment.end,
 
                 //Alinea al alto 
                 crossAxisAlignment: CrossAxisAlignment.end,
-                children: const [
-                  Text("5 + 7",
+                children: [
+                  Text(operacion,
                   style: TextStyle(color: Colors.white, fontSize: 25),
                   ),
 
                   SizedBox(height: 10),
                   Text(
-                    operacion,
+                    resultado,
                     style: TextStyle(
                       color: Colors.white,
                       fontSize: 25,
@@ -50,15 +54,69 @@ class _CalculadoraState extends State<Calculadora>{
               ),
             ),
             ),
-            Row(children: [boton("7"), boton("8"),  boton ("9"), boton("/")]),
+            Row(children: [boton("CA"), boton("CE"),  boton ("←"), boton("")]),
+            Row(children: [boton("7") , boton("8"),  boton ("9"), boton("/")]),
             Row(children: [boton("4") , boton("5"),  boton ("6"), boton("*")]),
+            Row(children: [boton("1") , boton("2"),  boton ("3"), boton("-")]),
+            Row(children: [boton("0") , boton("."),  boton ("="), boton("+")]),
+
           ],
       ))
 
     );
   }
+
+  void actionBoton(String valor){
+    //print(valor);
+    //Actualizarse
+    setState(() {
+      switch (valor) {
+        case "CE":
+          // Borra absolutamente todo
+          operacion = "";
+          break;
+
+        case "CA":
+          // Limpia la operación actual (puedes dejar el resultado anterior intacto)
+          operacion = "";
+          resultado = "0";
+          break;
+
+        case "←":
+          // Elimina solo el último carácter si la cadena no está vacía
+          if (operacion.isNotEmpty) {
+            operacion = operacion.substring(0, operacion.length - 1);
+          }
+          break;
+
+        case "+":
+        case "-":
+        case "*":
+        case "/":
+          if(numeroActual.isNotEmpty){
+            elementos.add(numeroActual);
+            elementos.add(valor);
+            numeroActual = "";
+            operacion += valor;
+          }
+          print(elementos);
+
+          default:
+              operacion += valor; 
+              numeroActual += valor;
+          break;
+      }
+});
+}
+
+
+  
   Widget boton (String texto){
     return Expanded(
-      child: ElevatedButton(onPressed: () {}, child: Text(texto)));
+      child: ElevatedButton(
+        onPressed: () {
+          actionBoton(texto);
+        }, 
+        child: Text(texto)));
   }
 }
